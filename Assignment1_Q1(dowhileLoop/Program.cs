02beacdeabf60a59a51972622225462e2aeaa69e
M7MDSG76 <mohammed.s.alghanmi@gmail.com>
2021-12-31 20:49:41 +0300

    Add project files.

---
+++ Assignment1_Q1(dowhileLoop/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Assignment1_Q1_dowhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int i = 0;
+            do
+            {
+                int j = 1;
+                int Sum = i*j;
+                do
+                {
+                    Console.WriteLine(i + " X " + j  + "=" + Sum);
+                        j++;
+                        Sum = i * j;
+                } while (j <= i);
+                 i++;
+            } while (i <= 10);
+        }
+    }
+}
